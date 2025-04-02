af6781f990a208813c2a93d3228d17c7904e40a5
John Ritter <john.ritter.8788@gmail.com>
2025-04-02 18:04:49 -0400

    Finished base code, needs lots of further revision

--- app.js
+++ app.js
@@ -41,6 +41,36 @@ document.addEventListener('DOMContentLoaded', () =>{
         gameDisplay.appendChild(obstacle);
         gameDisplay.appendChild(topObstacle);
         obstacle.style.left = obstacleLeft + 'px';
+        obstacle.style.bottom = obstacleBottom + 'px';
+        topObstacle.style.bottom = obstacleBottom + gap + 'px';
 
+        function moveObstacle(){
+            obstacleLeft -= 2;
+            obstacle.style.left = obstacleLeft + 'px';
+            topObstacle.style.left = obstacleLeft + 'px';
+            if(obstacleLeft === -60){
+                clearInterval(timerId);
+                gameDisplay.removeChild(obstacle);
+                gameDisplay.removeChild(topObstacle);
+            }
+            if(
+                obstacleLeft > 200 && obstacleLeft < 200 && birdLeft === 220 && (birdBottom < obstacleBottom + 153 || birdBottom > obstacleBottom + gap-200) || birdBottom === 0
+            ){
+                gameOver();
+                clearInterval(timerId);
+            }
+        }
+        let timerId = setInterval(moveObstacle, 20);
+        if(!isGameOver) setTimeout(generateObstacle, 3000);
+    }
+    generateObstacle();
+
+    function gameOver(){
+        clearInterval(gameTimerId);
+        console.log('game over');
+        isGameOver = true;
+        document.removeEventListener('keyup', control);
+        ground.classList.add('ground');
+        ground.classList.remove('ground-moving');
     }
 })
